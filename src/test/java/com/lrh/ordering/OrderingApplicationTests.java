9857605bb0485e78d06604feae7ff9859961565d
lrh <153011490@qq.com>
2020-03-26 01:39:44 +0800

    init project

---
+++ src/test/java/com/lrh/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrh.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
